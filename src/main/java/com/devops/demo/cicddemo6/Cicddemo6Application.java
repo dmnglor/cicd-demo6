6b8685d86e44ffeebe1724a566329ad6d62837cb
dmnglore <35909263+dmnglor@users.noreply.github.com>
2023-07-24 10:23:25 +0530

    first commit

---
+++ src/main/java/com/devops/demo/cicddemo6/Cicddemo6Application.java
@@ -0,0 +1,13 @@
+package com.devops.demo.cicddemo6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicddemo6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cicddemo6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/demo/cicddemo6/Cicddemo6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.demo.cicddemo6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicddemo6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
